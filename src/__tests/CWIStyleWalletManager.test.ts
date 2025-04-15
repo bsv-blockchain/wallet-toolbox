2d57768eeac3ce89ee61e469ae76d7ee0c81be87
Ty J Everett <ty@tyweb.us>
2025-04-15 11:27:02 -0700

    Fix some tests, still WIP

--- src/__tests/CWIStyleWalletManager.test.ts
+++ src/__tests/CWIStyleWalletManager.test.ts
@@ -305,7 +305,7 @@ describe('CWIStyleWalletManager Tests', () => {
 
     test('Throws if no token found by recovery key hash', async () => {
       ;(mockUMPTokenInteractor.findByRecoveryKeyHash as any).mockResolvedValueOnce(undefined)
-      await expect(manager.provideRecoveryKey(recoveryKey)).rejects.toThrow('No user found with this key')
+      await expect(manager.provideRecoveryKey(recoveryKey)).rejects.toThrow('No user found with this recovery key')
     })
   })
 
@@ -349,7 +349,7 @@ describe('CWIStyleWalletManager Tests', () => {
 
     test('Throws error if saving snapshot while no primary key or token set', async () => {
       // Manager is not yet authenticated
-      expect(() => manager.saveSnapshot()).toThrow('No primary key or current UMP token set')
+      expect(() => manager.saveSnapshot()).toThrow('No root primary key or current UMP token set')
     })
 
     test('Throws if snapshot is corrupt or cannot be decrypted', async () => {
@@ -380,7 +380,7 @@ describe('CWIStyleWalletManager Tests', () => {
     })
 
     test('Throws if not authenticated', async () => {
-      await expect(manager.changePassword('test-password')).rejects.toThrow('Not authenticated.')
+      await expect(manager.changePassword('test-password')).rejects.toThrow('Not authenticated or missing required data.')
     })
   })
 
@@ -407,7 +407,7 @@ describe('CWIStyleWalletManager Tests', () => {
     })
 
     test('Throws if not authenticated', async () => {
-      await expect(manager.changeRecoveryKey()).rejects.toThrow('Not authenticated.')
+      await expect(manager.changeRecoveryKey()).rejects.toThrow('Not authenticated or missing required data.')
     })
   })
 
