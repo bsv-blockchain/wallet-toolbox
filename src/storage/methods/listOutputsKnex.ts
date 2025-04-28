d0fb69c0fe2d4238e8b82da0d432a8912a88ff4b
Craig Porter <craigaporter@hotmail.com>
2025-04-29 00:40:18 +0100

    minor prettier bits

--- src/storage/methods/listOutputsKnex.ts
+++ src/storage/methods/listOutputsKnex.ts
@@ -124,12 +124,14 @@ export async function listOutputs(
   const noTags = tagIds.length === 0
   const includeSpent = specOp && specOp.includeSpent ? specOp.includeSpent : false
 
-  const txStatusOk = dsk.dbtype === 'PostgreSQL'
-    ? `(select status as tstatus from transactions where transactions."transactionId" = outputs."transactionId") in ('completed', 'unproven', 'nosend')`
-    : `(select status as tstatus from transactions where transactions.transactionId = outputs.transactionId) in ('completed', 'unproven', 'nosend')`
-  const txStatusOkCteq = dsk.dbtype === 'PostgreSQL'
-    ? `(select status as tstatus from transactions where transactions."transactionId" = o."transactionId") in ('completed', 'unproven', 'nosend')`
-    : `(select status as tstatus from transactions where transactions.transactionId = o.transactionId) in ('completed', 'unproven', 'nosend')`
+  const txStatusOk =
+    dsk.dbtype === 'PostgreSQL'
+      ? `(select status as tstatus from transactions where transactions."transactionId" = outputs."transactionId") in ('completed', 'unproven', 'nosend')`
+      : `(select status as tstatus from transactions where transactions.transactionId = outputs.transactionId) in ('completed', 'unproven', 'nosend')`
+  const txStatusOkCteq =
+    dsk.dbtype === 'PostgreSQL'
+      ? `(select status as tstatus from transactions where transactions."transactionId" = o."transactionId") in ('completed', 'unproven', 'nosend')`
+      : `(select status as tstatus from transactions where transactions.transactionId = o.transactionId) in ('completed', 'unproven', 'nosend')`
 
   const makeWithTagsQueries = () => {
     let cteqOptions = ''
